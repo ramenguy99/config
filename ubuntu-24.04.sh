3476edea6784f49b79826f15d31493dd26513d45
Dario Mylonopoulos <dmylos@yahoo.it>
2024-07-29 00:07:21 +0200

    Fixed vscode and added miniconda

--- ubuntu-24.04.sh
+++ ubuntu-24.04.sh
@@ -98,6 +98,16 @@ then
     sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 fi
 
+# Install miniconda
+if ! command -v conda &> /dev/null
+then
+    mkdir -p ~/miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm -rf ~/miniconda3/miniconda.sh
+    ~/miniconda3/bin/conda init bash
+fi
+
 # Install vscode
 if ! command -v code &> /dev/null
 then
